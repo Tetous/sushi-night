79a88dd1123d2000376f37311779779f8406aee3
Mateo Carriquí <mateocarriqui7@gmail.com>
2021-02-07 00:32:19 -0300

    Added search
    todo: pagination

--- src/components/AnimePoster.js
+++ src/components/AnimePoster.js
@@ -10,12 +10,13 @@ import {
 import { Badge } from "react-native-paper";
 import { formatEpisodes } from "../util";
 
+//anime entry of an anime list.
 export class EntryAnimePoster extends PureComponent {
   render() {
     const { entry, onPress } = this.props;
     const episodes = formatEpisodes(entry);
-    const coverImgUrl = entry.media.coverImage.extraLarge || "";
-    const title = entry.media.title.userPreferred || "";
+    const coverImgUrl = entry.media.coverImage.extraLarge;
+    const title = entry.media.title.userPreferred;
 
     return (
       <View style={styles.container}>
@@ -33,6 +34,41 @@ export class EntryAnimePoster extends PureComponent {
               }}
             >
               {episodes}
+            </Badge>
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.title} numberOfLines={1}>
+          {title}
+        </Text>
+      </View>
+    );
+  }
+}
+
+//searching for an anime will output this component.
+export class SearchAnimePoster extends PureComponent {
+  render() {
+    const { anime, onPress } = this.props;
+    const coverImgUrl = anime.coverImage.extraLarge;
+    const title = anime.title.userPreferred;
+    const seasonAndYear = `${anime.season} ${anime.seasonYear}`;
+
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={{ alignItems: "center" }}
+          onPress={() => onPress()}
+        >
+          <View>
+            <Image source={{ uri: coverImgUrl }} style={styles.pvAnimeImage} />
+            <Badge
+              style={{
+                position: "absolute",
+                backgroundColor: "#222831",
+                fontSize: 16,
+              }}
+            >
+              {seasonAndYear}
             </Badge>
           </View>
         </TouchableOpacity>
--- src/stacks/Search.js
+++ src/stacks/Search.js
@@ -1,27 +1,86 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Text, View } from "react-native";
-
+import { Text, View, FlatList, Platform } from "react-native";
+import { ActivityIndicator, Searchbar } from "react-native-paper";
+import { AuthContext } from "../providers/AuthProvider";
+import { Center } from "../components/Center";
+import { SearchAnimePoster } from "../components/AnimePoster";
 
 const Stack = createStackNavigator();
+const numColumns = Platform.OS === "web" ? 6 : 3;
 
 const Main = ({ navigation }) => {
-  return(
+  const { client } = useContext(AuthContext);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [fetching, setFetching] = useState(false);
+  const [results, setResults] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const search = async () => {
+    if (searchQuery.length > 3) {
+      setFetching(true);
+      console.log(searchQuery);
+      await client
+        .searchAnime(searchQuery, {page,perPage:25})
+        .then((data) => {
+          setResults(data);
+          console.log(JSON.stringify(data));
+        })
+        .catch((err) => console.log(err));
+      setFetching(false);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <SearchAnimePoster
+      anime={item}
+      onPress={() => {
+        //pass an extra param like needsUserFetchingData: true.
+        navigation.navigate("Anime", {
+          screen: "AnimeDetails",
+          params: {
+            anime: item,
+          },
+        });
+      }}
+    />
+  );
+
+  return (
+    <View style={{ flex: 1 }}>
       <View>
-          <Text>
-              Hello from Search
-          </Text>
+        <Searchbar
+          placeholder="search for an anime"
+          onChangeText={(text) => setSearchQuery(text)}
+          value={searchQuery}
+          onSubmitEditing={() => search()}
+          onIconPress={() => search()}
+        />
       </View>
-  )
-}
+      <View style={{ flex: 1 }}>
+        {fetching ? (
+          <Center>
+            <ActivityIndicator size="large" color="#00adb5" />
+          </Center>
+        ) : results.length ? (
+          <FlatList
+            style={{ width: "100%", flex: 1 }}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
+            data={results}
+            numColumns={numColumns}
+            disableVirtualization={true}
+          />
+        ) : null}
+      </View>
+    </View>
+  );
+};
 
 export const SearchStack = ({}) => {
   return (
-    <Stack.Navigator initialRouteName="Main">
-      <Stack.Screen
-        name="Main"
-        component={Main}
-      />
+    <Stack.Navigator initialRouteName="Main" headerMode="none">
+      <Stack.Screen name="Main" component={Main} />
     </Stack.Navigator>
   );
 };
